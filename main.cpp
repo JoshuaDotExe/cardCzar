00698eabc974d9bc04d5b90c8f861c363dcd2b57
JoshuaDotExe <joshuawilliamswebster@gmail.com>
2023-01-24 16:46:58 +0000

    init

--- main.cpp
+++ main.cpp
@@ -6,18 +6,37 @@
 #include <iterator>
 #include <vector>
 
+#include <sqlite3.h>
+
 #include "deck.h"
 
 
 int main(){
     srand(time(NULL));
+    // sqlite3 *db;
+    // char *zErrMsg=0;
+    int pArr[52][52];
+    for(int i=0; i<52; i++){
+        for(int j=0; j<52; j++){
+            pArr[i][j]=0;
+        }
+    }
     deck cards;
     cards.build();
-    //std::vector<bool> tempVec = cards.exportBits();
-    for (int i=0; i<1000000; i++){
+    int counter;
+    // std::vector<bool> tempVec = cards.exportBits();
+    for (int i=0; i<1000; i++){
         cards.reshuffle();  // 2.2 seconds
-        
-        //cards.printDeck();
+        counter=0;
+        for(int num: cards.cards){
+            pArr[counter][num]+=1;
+            counter++;
+        }
+        // cards.printDeck();
+        // int counter = 0;
+        // std::cout << std::endl;
     }
     std::cout << std::endl;
 }
+
+
